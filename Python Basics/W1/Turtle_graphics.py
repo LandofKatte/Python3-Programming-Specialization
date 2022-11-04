cab37dea186bf8292ca39fbe3ab3a058e28631ae
LandofKatte <103328611+LandofKatte@users.noreply.github.com>
2022-11-03 23:36:09 -0400

    Create Turtle_graphics.py

---
+++ Python Basics/W1/Turtle_graphics.py
@@ -0,0 +1,89 @@
+#The first line tells Python to load a module named turtle. That module brings us two new types that we can use: the Turtle type, and the Screen type. The dot notation turtle.Turtle means “The Turtle type that is defined within the turtle module”. (Remember that Python is case sensitive, so the module name, turtle, with a lowercase t, is different from the type Turtle because of the uppercase T.)
+
+#We then create and open what the turtle module calls a screen (we would prefer to call it a window, or in the case of this web version of Python simply a canvas), which we assign to variable wn. Every window contains a canvas, which is the area inside the window on which we can draw.
+
+#Which direction does the Turtle face when it is created? *EAST*
+#Why do we type turtle.Turtle() to get a new Turtle object? *The first "turtle" (before the period) tells Python that we are referring to the turtle module, which is where the object "Turtle" is found.*
+
+
+#Example given
+
+import turtle             # allows us to use the turtles library
+wn = turtle.Screen()      # creates a graphics window
+alex = turtle.Turtle()    # create a turtle named alex
+alex.forward(150)         # tell alex to move forward by 150 units
+alex.left(90)             # turn by 90 degrees
+alex.forward(75)          # complete the second side of a rectangle
+
+#unfinished rectangle
+
+#Example given
+
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")        # set the window background color
+
+tess = turtle.Turtle()
+tess.color("blue")              # make tess blue
+tess.pensize(3)                 # set the width of her pen
+
+tess.forward(50)
+tess.left(120)
+tess.forward(50)
+
+wn.exitonclick()                # wait for a user click on the canvas
+
+#The last line plays a very important role. The wn variable refers to the window shown above. When we invoke its exitonclick method, the program pauses execution and waits for the user to click the mouse somewhere in the window. When this click event occurs, the response is to close the turtle window and exit (stop execution of) the Python program.
+
+Color Names Available:
+
+https://www.w3schools.com/colors/colors_names.asp
+  
+#Example given (2 turtles)
+
+import turtle
+wn = turtle.Screen()             # Set up the window and its attributes
+wn.bgcolor("lightgreen")
+
+
+tess = turtle.Turtle()           # create tess and set his pen width
+tess.pensize(5)
+
+alex = turtle.Turtle()           # create alex
+alex.color("hotpink")            # set his color
+
+tess.forward(80)                 # Let tess draw an equilateral triangle
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)                   # complete the triangle
+
+tess.right(180)                  # turn tess around
+tess.forward(80)                 # move her away from the origin so we can see alex
+
+alex.forward(50)                 # make alex draw a square
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+
+wn.exitonclick()
+
+________________________________________________________________________
+
+import turtle             # allows us to use the turtles library
+wn = turtle.Screen()      # creates a graphics window
+alex = turtle.Turtle()    # create a turtle named alex
+alex.left(50)
+alex.forward(133)         # tell alex to move forward by 133 units
+alex.circle(50,200)             # turn
+alex.right(140)
+alex.circle(50,200)
+alex.forward(133)
+
+#creates a heart :)
