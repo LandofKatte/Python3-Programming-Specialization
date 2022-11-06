ad6f27d92b561db85fa03aa2a8580e2581cd2a98
LandofKatte <103328611+LandofKatte@users.noreply.github.com>
2022-11-05 22:58:15 -0400

    Create forLoop.py

---
+++ Python Basics/W1/forLoop.py
@@ -0,0 +1,6 @@
+#Write a program that prints We like Python's turtles! 100 times.
+
+for _ in range(100):
+    print("We like Python's turtles!")
+    
+_______________________________________________
